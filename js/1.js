1808c1da280c82ba709941dd33712cdd80868cbc
VlA211 <114938633+VlA211@users.noreply.github.com>
2024-06-09 21:31:52 +0300

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -17,3 +17,37 @@ for (var i = 0; i < coll.length; i++) {
 function scrollDown(str) {
     document.getElementById(str).scrollIntoView({behavior: 'smooth'})
 }
+
+// script.js
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Scroll down
+        header.style.top = '-100px'; // Adjust according to header height
+    } else {
+        // Scroll up
+        header.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.container .list .num');
+
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    });
+});
+
+
+
+
